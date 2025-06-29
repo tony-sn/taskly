dbda92c8d0ca011c38dbd7b39d23446d3698cc70
Tony Nguyen <57897153+tony-sn@users.noreply.github.com>
2025-06-29 13:30:10 +1000

    feat: add delete button and formatting code

--- theme.ts
+++ theme.ts
@@ -1,4 +1,7 @@
 export const theme = {
-    colorCerulean: "#1a759f",
-    colorWhite: "#ffffff"
-}
+  colorCerulean: "#1a759f",
+  colorWhite: "#ffffff",
+  colorBlack: "#000000",
+  colorRed: "red",
+};
+
