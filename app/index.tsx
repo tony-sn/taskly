dd3cf53294a97c20ea3c3080238fe2e09643d746
Tony Nguyen <57897153+tony-sn@users.noreply.github.com>
2025-07-06 23:48:07 +1000

    feat: add list, scroll view

--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,55 @@
-import { StyleSheet, View } from "react-native";
+import { useState } from "react";
+import { ScrollView, StyleSheet, TextInput } from "react-native";
+
 import ShoppingListItem from "../components/ShoppingListItem";
-import { Link } from "expo-router";
+import { theme } from "../theme";
+
+type ShoppingListItemType = {
+  id: string;
+  name: string;
+};
+
+const initialList: ShoppingListItemType[] = [
+  { id: "1", name: "Coffee" },
+  { id: "2", name: "Tea" },
+  { id: "1", name: "Milk" },
+];
 
 export default function App() {
+  const [shoppingList, setShoppingList] =
+    useState<ShoppingListItemType[]>(initialList);
+  const [value, setValue] = useState("");
+
+  const handleSubmit = () => {
+    const newShoppingList = [
+      ...shoppingList,
+      {
+        id: new Date().toTimeString(),
+        name: value,
+      },
+    ];
+    setShoppingList(newShoppingList);
+    setValue("");
+  };
   return (
-    <View style={styles.container}>
-      <Link
-        href="/counter"
-        style={{
-          textAlign: "center",
-          marginBottom: 18,
-        }}
-      >
-        Go to counter
-      </Link>
-      <ShoppingListItem name="Coffee" />
-      <ShoppingListItem name="Tea" isCompleted />
-      <ShoppingListItem name="Milk" isCompleted />
-    </View>
+    <ScrollView
+      style={styles.container}
+      contentContainerStyle={styles.contentContainer}
+      stickyHeaderIndices={[0]}
+    >
+      <TextInput
+        placeholder="E.g. Coffee"
+        style={styles.textInput}
+        value={value}
+        onChangeText={setValue}
+        returnKeyType="done"
+        onSubmitEditing={handleSubmit}
+      />
+
+      {shoppingList.map((item) => (
+        <ShoppingListItem key={item.id} name={item.name} />
+      ))}
+    </ScrollView>
   );
 }
 
@@ -25,6 +57,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    justifyContent: "center",
+    padding: 12,
+  },
+  contentContainer: {
+    paddingBottom: 24,
+  },
+  textInput: {
+    borderColor: theme.colorLightGrey,
+    borderWidth: 2,
+    padding: 12,
+    marginHorizontal: 12,
+    marginBottom: 12,
+    fontSize: 18,
+    borderRadius: 50,
+    backgroundColor: theme.colorWhite,
   },
 });
